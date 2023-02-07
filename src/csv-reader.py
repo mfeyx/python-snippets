050434cdcc2140a8e8050a9871d10d0cd85287c3
Markus Feiks <github@feyx.de>
2023-02-07 21:49:31 +0100

    json export

--- src/csv-reader.py
+++ src/csv-reader.py
@@ -10,16 +10,17 @@ fp = os.path.join(data_folder, file_name)
 d = {}
 keys = None
 rows = []
-with open(fp, "r", encoding = "utf-8") as f:
+with open(fp, "r", encoding="utf-8") as f:
     csvfile = csv.reader(f, dialect="excel", delimiter=",", quotechar='"')
     for i, row in enumerate(csvfile):
         if i == 0:
             keys = row
-            d = {k: None for k in keys}
+            d = {k: None for k in row}
 
         if row[0] in ("3669775299", "3669995209"):
+            # print(row)
             row_dict = {**d, **{k:v for k, v in zip(keys, row)} }
             rows.append(row_dict)
 
     with open(os.path.join(data_folder, "deal.json"), "w", encoding="utf-8") as f:
-        json.dump(rows, f)
+        json.dump(rows, f, ensure_ascii=False, indent=4)
